20e7ed886d6e3bbe21e1a9383f398de70e4b3d36
sararesendes <anasara.resendes@gmail.com>
2026-02-28 10:38:43 -0300

    Delete Aula11/Aula11_Ex1.py

--- Aula11/Aula11_Ex1.py
+++
@@ -1,8 +0,0 @@
-#Exercício 1:
-#Peça ao usuário para inserir um número e manipule a exceção caso ele insira algo que não seja um número inteiro.
-
-try:
-    numero = int(input("Digite um número inteiro: "))
-    print("Você digitou o número:", numero)
-except ValueError:
-    print("Erro! Você não digitou um número inteiro.")
